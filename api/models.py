ff6517a2fb61a472352cfb6956faa38982329c7c
Mikdhad.k <mikdhadk32@gmail.com>
2024-02-07 00:21:23 +0530

    serializer

--- api/models.py
+++ api/models.py
@@ -5,5 +5,10 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=100)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
