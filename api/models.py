9f3841287b27a6addda6455591422791e5ef47a7
Mikdhad.k <mikdhadk32@gmail.com>
2024-02-07 10:23:24 +0530

    setup for media

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    creator = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
     
    
 
--- postApp/urls.py
+++ postApp/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import statistics
 from django.contrib import admin
 from django.urls import path,include
+
+from postApp import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls'))
-]
+]+statistics(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
